cc80722f010bfd08e3ba5c112e58a8a1f94fdaa3
AlexMitiukov <151469238+AlexMitiukov@users.noreply.github.com>
2025-03-04 20:30:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Лабораторные работы</title>
+</head>
+<body>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л21/index.php'">Лабораторная работа №21</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z1.local/index.php'">Лабораторная работа №22. Задание 1</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z2.local/index.php'">Лабораторная работа №22. Задание 2</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z3.local/index.php'">Лабораторная работа №22. Задание 3</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z4.local/index.php'">Лабораторная работа №22. Задание 4</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z5.local/index.php'">Лабораторная работа №22. Задание 5</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л22/lr22-z6.local/index.php'">Лабораторная работа №22. Задание 6</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л23/lr23.local/index.php'">Лабораторная работа №23</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л24/lr24.local/index.php'">Лабораторная работа №24</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л25/lr25.local/index.php'">Лабораторная работа №25</button><br><br>
+
+    <button onclick="window.location.href='ВЫПОЛНЕНО/л26-27/lr26-27.local/index.php'">Лабораторная работа №26-27</button><br><br>
+
+</body>
+</html>
