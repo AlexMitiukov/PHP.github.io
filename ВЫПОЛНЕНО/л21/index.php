c6c43682b7c30e9ba1696d15a9f706226f6c80a6
AlexMitiukov <151469238+AlexMitiukov@users.noreply.github.com>
2025-03-04 20:37:30 +0300

    Add files via upload

---
+++ ВЫПОЛНЕНО/л21/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ссылки на PHP-скрипты</title>
+    </head>
+    <body>
+        <h1>Ссылки на PHP-скрипты</h1>
+        <ul>
+            <li><a href="phpinfo.php">Информация о PHP (phpinfo)</a></li>
+            <li><a href="greeting.php">Приветствие</a></li>
+            <li><a href="colored_name.php">ФИО разработчика с цветом и размером</a></li>
+            <li><a href="constant_example.php">Пример использования констант</a></li>
+        </ul>
+    </body>
+</html>
